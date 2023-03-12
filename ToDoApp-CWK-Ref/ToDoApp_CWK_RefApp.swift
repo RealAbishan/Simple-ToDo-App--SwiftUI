54b6d35d4e7239b5cd6617109065b723e273f217
Abishan <highoncode09@gmail.com>
2023-03-12 20:03:52 +0530

    Initial Commit

---
+++ ToDoApp-CWK-Ref/ToDoApp_CWK_RefApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_CWK_RefApp.swift
+//  ToDoApp-CWK-Ref
+//
+//  Created by Abishan Parameswaran on 2023-03-12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp_CWK_RefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
